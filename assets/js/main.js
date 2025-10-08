04ccbaa931114e20dc9e5b2c3dd3f222c122943b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-08 12:48:32 -0500

    add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,17 @@ import { animate, splitText, stagger } from 'animejs';
 const { chars: chars1 } = splitText('.home__profession-1', { chars: true, });
 const { chars: chars2 } = splitText('.home__profession-2', { chars: true, });
 
+animate(chars1, {
+    y: [
+        { to: ['100%', '0%'] },
+        { to: '-100%', delay: 750, ease: 'in(3)' }
+    ],
+    duration: 750,
+    ease: 'out(3)',
+    delay: stagger(50),
+    loop: true,
+});
+
 /*=============== SWIPER PROJECTS ===============*/
 
 

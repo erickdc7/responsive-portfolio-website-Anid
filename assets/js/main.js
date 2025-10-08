99d4189c7815ec08f1efe2399079cac18a963ef2
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-08 12:53:03 -0500

    fix code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 /*=============== HOME SPLIT TEXT ===============*/
-import { animate, splitText, stagger } from 'animejs';
+const { animate, text, stagger } = anime
 
-const { chars: chars1 } = splitText('.home__profession-1', { chars: true, });
-const { chars: chars2 } = splitText('.home__profession-2', { chars: true, });
+const { chars: chars1 } = text.split('.home__profession-1', { chars: true, });
+const { chars: chars2 } = text.split('.home__profession-2', { chars: true, });
 
 animate(chars1, {
     y: [
